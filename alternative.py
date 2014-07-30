b812ad455c455e1094d9083613d0c125d5ac60a7
warholm <per.warholm@scilifelab.se>
2014-07-30 15:48:52 +0200

    Added probability calculations of phosphorylation sites data.

--- alternative.py
+++ alternative.py
@@ -4,7 +4,7 @@ multiple splice variants. Count the number of splice variants. Map
 phosphosites from tissue data to a parsed proteome.
 
 per.warholm@scilifelab.se
-2014-07-29
+2014-07-30
 """
 
 from Bio import SeqIO
@@ -95,7 +95,8 @@ def map_phosposites(tissues, proteome, translation):
                 # print protein_name, hits, n
                 
                 ## save result in dictionary
-                phosphosites[tissue_name][protein_name] = [float(i)/n for i in hits]
+                # phosphosites[tissue_name][protein_name] = [float(i)/n for i in hits]
+                phosphosites[tissue_name][protein_name] = hits, n
 
     return phosphosites
 
@@ -115,14 +116,86 @@ def determine_tissue_specificity(phosphosites):
             tissue_specificity[protein] = tissue_specific
     return tissue_specificity
 
+def probabilities(proteome, translation_table, phosphosites, tissue_specificity):
+    """Calculate probabilities according to project notes."""
+    
+    ## First probability:
+    ## Pr(A | "Gene is alt.spliced") > Pr(A | "Gene is not alt.spliced")
+    ## A = "Gene has a phosphosite"
+    
+    ## count alternative slicing
+    n_genes_AS = 0
+    n_genes_nonAS = 0
+    for k, v in proteome.iteritems():
+        if len(v) > 1:
+            n_genes_AS+=1
+        elif len(v) == 1:
+            n_genes_nonAS+=1
+    
+    ## conditional count of phosphosites
+    count_phosphogenes = []
+    A_given_AS = []
+    A_given_nonAS = []
+    for tissue, data in phosphosites.iteritems():
+        for protein, phosphosite_expression in data.iteritems():
+            count_phosphogenes.append(protein)
+            if len(proteome[translation_table[protein]]) > 1:
+                A_given_AS.append(protein)
+            elif len(proteome[translation_table[protein]]) == 1:
+                A_given_nonAS.append(protein)
+    
+    ## counts
+    n_genes = len(proteome)
+    n_phosphogenes = len(set(count_phosphogenes))
+    n_A_given_AS = len(set(A_given_AS))
+    n_A_given_nonAS = len(set(A_given_nonAS))
+    
+    ## probabilities
+    P_phosposite = float(n_phosphogenes)/n_genes
+    P_A_given_AS = float(n_A_given_AS)/n_genes_AS
+    P_A_given_nonAS = float(n_A_given_nonAS)/n_genes_nonAS
+    
+    ## lambda pseudo function for rounding off results
+    rnd = lambda x: '{0:.3f}'.format(x)
+    
+    print 'First probability:'
+    print 'Pr(A | "Gene is alt.spliced")', rnd(P_A_given_AS)
+    print 'Pr(A | "Gene is not alt.spliced")', rnd(P_A_given_nonAS)
+    print 'Ratio:', rnd(P_A_given_AS/P_A_given_nonAS)
+    
+    
+    ## Second Probability:
+    ## Pr(B | "Gene is alt.spliced") > Pr(B | "Gene is not alt.spliced")
+    ## B: The genes have phosphosites that are unique for one of the tissues
+    
+    ## filter away proteins that aren't tissue specific
+    n_B_given_AS = 0
+    for protein in set(A_given_AS):
+        if tissue_specificity[protein]:
+            n_B_given_AS+=1
+    n_B_given_nonAS = 0
+    for protein in set(A_given_nonAS):
+        if tissue_specificity[protein]:
+            n_B_given_nonAS+=1
+    
+    ## probabilities
+    P_B_given_AS = float(n_B_given_AS)/n_genes_AS
+    P_B_given_nonAS = float(n_B_given_nonAS)/n_genes_nonAS
+    
+    print '\nSecond probability:'
+    print 'Pr(B | "Gene is alt.spliced")', rnd(P_B_given_AS)
+    print 'Pr(B | "Gene is not alt.spliced")', rnd(P_B_given_nonAS)
+    print 'Ratio:', rnd(P_B_given_AS/P_B_given_nonAS)
+
+## locations of input data
+fn_fasta = '../../data/mouse/Mus_musculus.GRCm38.75.pep.all.fa'
+# fn_tissues = ['../data/mouse/tissues/brain-identified-proteins.txt']
+fn_tissues = sorted(glob.glob('../../data/mouse/tissues/*-identified-proteins.txt'))
 
 ## main pipeline
-fn_fasta = '../data/mouse/Mus_musculus.GRCm38.75.pep.all.fa'
-# fn_tissues = ['../data/mouse/tissues/brain-identified-proteins.txt']
-fn_tissues = sorted(glob.glob('../data/mouse/tissues/*-identified-proteins.txt'))
-
 mouse_proteome, translation_table = parse_proteome(fn_fasta)
 # count_splice_variants(mouse_proteome)
-# count_splice_variants_average(mouse_proteome)
+n = count_splice_variants_average(mouse_proteome)
 phosphosites = map_phosposites(fn_tissues, mouse_proteome, translation_table)
 tissue_specificity = determine_tissue_specificity(phosphosites)
+probabilities(mouse_proteome, translation_table, phosphosites, tissue_specificity)
